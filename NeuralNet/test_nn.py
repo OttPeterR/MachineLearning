f9aeb061e16ca30e9ac0246c8ee720feb263c9ec
Peter Ott <ott.peter.r@gmail.com>
2017-09-24 16:00:00 -0400

    added test_back_propagate

--- NeuralNet/test_nn.py
+++ NeuralNet/test_nn.py
@@ -6,14 +6,30 @@ def test_make_h():
     v = np.matrix([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]])
     i = np.matrix([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]])
     ans = nn.make_h(v, i)
-    print ans
+    print(ans)
 # test_make_h()
 # passed
 
 
+def test_back_propagate():
+    datum = [np.matrix([[.1], [.5], [.9]]),
+             np.matrix([.9])]
+    alpha = 1
+    v = np.matrix([[.5, .5, .5],
+                   [.5, .5, .5],
+                   [.5, .5, .5],
+                   [.5, .5, .5]])
+    w = np.matrix([[.5, .5, .5, .5]])
+    ans = nn.back_propagate(datum, alpha, v, w)
+    print(ans)
+
+# test_back_propagate()
+# passed
+
+
 def test_forward_propagate():
-    datum = [np.matrix([0.4, 0.15, 0.3]), np.matrix(
-        [0.25, 0.25, 0.25, 0.5, 0.25])]  # [input, output]
+    datum = [np.matrix([[0.4], [0.15], [0.3]]),
+             np.matrix([[0.25], [0.25], [0.25], [0.5], [0.25]])]
     v = np.matrix([[0.3, 0.3, 0.3],
                    [0.2, 0.3, 0.4],
                    [0.4, 0.4, 0.5],
@@ -24,7 +40,8 @@ def test_forward_propagate():
                    [0.1, 0.3, 0.4, 0.2],
                    [0.1, 0.3, 0.4, 0.2]])
     ans = nn.forward_propagate(datum, v, w)
-    print ans
+    print(ans)
 
 
-test_forward_propagate()
+# test_forward_propagate()
+# passed
