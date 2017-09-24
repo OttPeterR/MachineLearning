d1d4c9ae82227208b23bd8ec6b7dbef6d9dd8ffa
Peter Ott <ott.peter.r@gmail.com>
2017-09-24 15:44:42 -0400

    added test_make_h and formatting

--- NeuralNet/test_nn.py
+++ NeuralNet/test_nn.py
@@ -1,11 +1,30 @@
 import numpy as np
 import nn
 
+
+def test_make_h():
+    v = np.matrix([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]])
+    i = np.matrix([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]])
+    ans = nn.make_h(v, i)
+    print ans
+# test_make_h()
+# passed
+
+
 def test_forward_propagate():
-    datum = [np.matrix([0.4, 0.15, 0.3]), np.matrix([0.25, 0.25, 0.25, 0.5, 0.25])] #[input, output]
-    v = np.matrix([[0.3,0.3,0.3], [0.2,0.3,0.4], [0.4,0.4,0.5], [0.1,0.2,0.3]])
-    w = np.matrix([[0.2,0.1,0.4,0.6],[0.2,0.3,0.1,0.2],[0.2,0.4,0.4,0.2],[0.1,0.3,0.4,0.2],[0.1,0.3,0.4,0.2]])
+    datum = [np.matrix([0.4, 0.15, 0.3]), np.matrix(
+        [0.25, 0.25, 0.25, 0.5, 0.25])]  # [input, output]
+    v = np.matrix([[0.3, 0.3, 0.3],
+                   [0.2, 0.3, 0.4],
+                   [0.4, 0.4, 0.5],
+                   [0.1, 0.2, 0.3]])
+    w = np.matrix([[0.2, 0.1, 0.4, 0.6],
+                   [0.2, 0.3, 0.1, 0.2],
+                   [0.2, 0.4, 0.4, 0.2],
+                   [0.1, 0.3, 0.4, 0.2],
+                   [0.1, 0.3, 0.4, 0.2]])
     ans = nn.forward_propagate(datum, v, w)
     print ans
+
 
 test_forward_propagate()
