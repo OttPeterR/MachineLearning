9f6d3a749c317791680231f34e17e718dba01816
Peter Ott <ott.peter.r@gmail.com>
2017-10-22 03:37:55 -0400

    finished code (first draft)
    probably has bugs, needs to be tested

--- k-nearest-neighbor.py
+++ k-nearest-neighbor.py
@@ -1,6 +1,5 @@
 # K Nearest Neighbor
-
-import Math
+import math
 
 
 def count(a, b):
@@ -42,8 +41,20 @@ def maplist(func, lis):
     return [map(func, lis[i:]) for i in xrange(len(lis))]
 
 
-#def k_nearest_neighbor(examples, new_example, k=1, measure='count'):
-#    # a list of [ (distance, class) ... ]
-#    distance = maplist()
-#    neighbors = maplist()
+def k_nearest_neighbor(examples, new_example, k=1, measure='count'):
+    # a list of [ (distance, class) ... ]
+    distance = maplist(lambda e: [difference(e[0], new_example[0], measure)],
+                       examples)
 
+    # sort distance by least distance value
+    def getKey(pair):
+        return pair[0]
+    sorted_distance = sorted(distance, key=getKey)
+
+    # distance values are not needed anymore, just take the classes
+    neighbors = maplist(lambda pair: pair[1], sorted_distance)
+
+    # return the most common of the k nearest neighbors
+    neighbors = neighbors[0:k]
+    return max(set(neighbors), key=neighbors.count)
+    
